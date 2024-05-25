24d66095525fae04327144cc523f03f87051f47c
andrey <kobzev605@gmail.com>
2024-05-25 18:37:51 +0300

    feat: add link

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,18 @@ import CalendarEventTask from '~/components/widget/CalendarEventTask.vue'
 
 <template>
   <div>
+    <div class="flex w-full justify-end">
+      <div class="flex flex-col gap-2">
+        <nuxt-link to="/add-event" class="flex gap-2 items-center text-blue-500">
+          <Icon name="i-material-symbols-light-calendar-add-on-rounded" size="16"></Icon>
+          <p>Добавить Мероприятие</p>
+        </nuxt-link>
+        <nuxt-link to="/add-event" class="flex gap-2 items-center text-blue-500">
+          <Icon name="i-material-symbols-light-add-task-rounded" size="16"></Icon>
+          <p>Добавить Задачу</p>
+        </nuxt-link>
+      </div>
+    </div>
     <CalendarEventTask />
   </div>
 </template>
